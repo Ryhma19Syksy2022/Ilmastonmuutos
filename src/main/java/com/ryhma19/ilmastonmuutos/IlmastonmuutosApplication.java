f73d8a9ea0704a998400ca268fd3154b02b8cc70
JoniSalonen <t1sajo03@students.oamk.fi>
2022-11-01 13:29:49 +0200

    Alustus

---
+++ src/main/java/com/ryhma19/ilmastonmuutos/IlmastonmuutosApplication.java
@@ -0,0 +1,13 @@
+package com.ryhma19.ilmastonmuutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlmastonmuutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlmastonmuutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryhma19/ilmastonmuutos/IlmastonmuutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryhma19.ilmastonmuutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlmastonmuutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
