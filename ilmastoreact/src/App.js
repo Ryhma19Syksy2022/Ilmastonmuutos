a332080ad15c1540751f413f0c158a565abc56c7
Jani <jani@rannanjarvi.net>
2022-11-16 12:04:15 +0200

    added V3

--- ilmastoreact/src/App.js
+++ ilmastoreact/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Profile from './components/Profile';
+import V3 from './components/V3';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/v1' element={<V1Annual/>}></Route>
         <Route path='/v2' element={<V1Annual/>}></Route>
+        <Route path='/v3' element={<V3/>}></Route>
         <Route path='/profile' element={<Profile/>}></Route>
       </Routes>
     </div>
