f929aba289f726f6fcac27bb2e854f2d6357885c
JoniSalonen <t1sajo03@students.oamk.fi>
2022-11-18 18:00:24 +0200

    V8 alustus V9 rakenne muutos

--- ilmastoreact/src/App.js
+++ ilmastoreact/src/App.js
@@ -6,8 +6,9 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 import Profile from './components/Profile';
 import Login from './components/LogIn';
-import V9Sectors from './components/V9Sectors';
 import V3 from './components/V3';
+import V8 from './components/V8';
+import V9 from './components/V9';
 
 
 function App() {
@@ -18,8 +19,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/v1' element={<V1Annual/>}></Route>
-        <Route path='/v2' element={<V9Sectors/>}></Route>
-        <Route path='/v3' element={<V3/>}></Route>
+        <Route path='/v2' element={<V8/>}></Route>
+        <Route path='/v3' element={<V9/>}></Route>
         <Route path='Profile' element={<Profile/>}></Route>
         <Route path='/Login' element={<Login/>}></Route>
       </Routes>
