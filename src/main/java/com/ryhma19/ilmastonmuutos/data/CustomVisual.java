71d63be65fcea18da9edf12183e0697f4b34a246
JoniSalonen <101516095+JoniSalonen@users.noreply.github.com>
2022-12-12 14:44:42 +0200

    N3

---
+++ src/main/java/com/ryhma19/ilmastonmuutos/data/CustomVisual.java
@@ -0,0 +1,108 @@
+package com.ryhma19.ilmastonmuutos.data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customvisuals")
+
+public class CustomVisual {
+    @Id
+    private int id;
+    private String visual_Id;
+    private int Layout;
+    private String v1;
+    private String v3;
+    private String v5;
+    private String v6;
+    private String v7;
+    private String v8;
+    private String v9;
+
+    public CustomVisual(){
+
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVisual_Id() {
+        return this.visual_Id;
+    }
+
+    public void setVisual_Id(String visual_Id) {
+        this.visual_Id = visual_Id;
+    }
+
+    public int getLayout() {
+        return this.Layout;
+    }
+
+    public void setLayout(int Layout) {
+        this.Layout = Layout;
+    }
+
+    public String getV1() {
+        return this.v1;
+    }
+
+    public void setV1(String v1) {
+        this.v1 = v1;
+    }
+
+    public String getV3() {
+        return this.v3;
+    }
+
+    public void setV3(String v3) {
+        this.v3 = v3;
+    }
+
+    public String getV5() {
+        return this.v5;
+    }
+
+    public void setV5(String v5) {
+        this.v5 = v5;
+    }
+
+    public String getV6() {
+        return this.v6;
+    }
+
+    public void setV6(String v6) {
+        this.v6 = v6;
+    }
+
+    public String getV7() {
+        return this.v7;
+    }
+
+    public void setV7(String v7) {
+        this.v7 = v7;
+    }
+
+    public String getV8() {
+        return this.v8;
+    }
+
+    public void setV8(String v8) {
+        this.v8 = v8;
+    }
+
+    public String getV9() {
+        return this.v9;
+    }
+
+    public void setV9(String v9) {
+        this.v9 = v9;
+    }
+
+    
+}
---
+++ src/main/java/com/ryhma19/ilmastonmuutos/repository/CustomVisualRepository.java
@@ -0,0 +1,9 @@
+package com.ryhma19.ilmastonmuutos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ryhma19.ilmastonmuutos.data.CustomVisual;
+
+public interface CustomVisualRepository extends JpaRepository<CustomVisual, Integer> {
+    
+}
