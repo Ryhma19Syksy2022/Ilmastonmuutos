c5ea17b5bfae9a8b46444f63213c641b1305ccb0
0nd3 <onni.loponen@gmail.com>
2022-12-14 16:33:33 +0200

    pikkukorjaus

--- src/main/java/com/ryhma19/ilmastonmuutos/repository/CustomVisualRepository.java
+++ src/main/java/com/ryhma19/ilmastonmuutos/repository/CustomVisualRepository.java
@@ -1,5 +1,7 @@
 package com.ryhma19.ilmastonmuutos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ryhma19.ilmastonmuutos.data.CustomVisual;
