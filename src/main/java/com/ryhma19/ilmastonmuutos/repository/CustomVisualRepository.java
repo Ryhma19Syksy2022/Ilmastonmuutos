7e5fe255db9bde52705ef25f6733ba6065ad25be
0nd3 <onni.loponen@gmail.com>
2022-12-14 16:35:56 +0200

    pikkukorjaus

--- src/main/java/com/ryhma19/ilmastonmuutos/repository/CustomVisualRepository.java
+++ src/main/java/com/ryhma19/ilmastonmuutos/repository/CustomVisualRepository.java
@@ -1,6 +1,7 @@
 package com.ryhma19.ilmastonmuutos.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +9,7 @@ import com.ryhma19.ilmastonmuutos.data.CustomVisual;
 
 public interface CustomVisualRepository extends JpaRepository<CustomVisual, String> {
     
+    Optional<CustomVisual> findById(String visual_Id);
     List<CustomVisual> findByOwner(String owner);
 
 }
